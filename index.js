7053ccb72ba2d98af7f5aac82d1c727b1c11ac5d
Adrian Kiptoo <cherukip.adrian@gmail.com>
2025-02-26 12:54:43 +0300

    arithmetic

---
+++ index.js
@@ -0,0 +1,40 @@
+// Adds two numbers and returns the result
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Subtracts b from a and returns the result
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Multiplies two numbers and returns the result
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Divides a by b and returns the result
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  // Increments n and returns the result
+  function increment(n) {
+    return n + 1;
+  }
+  
+  // Decrements n and returns the result
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Parses n as an integer and returns the parsed integer
+  function makeInt(n) {
+    return parseInt(n, 10); // Assumes base 10
+  }
+  
+  // Parses n as a floating point number and returns the result
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
